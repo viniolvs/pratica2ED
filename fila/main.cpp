0f89f835f499eb94cb7a0a062846f635e992ebf6
Vinicius de Oliveira Silva <vinioliveira.s@hotmail.com>
2022-08-01 01:24:27 -0300

    Implementando testes para todos os métodos

--- fila/main.cpp
+++ fila/main.cpp
@@ -3,19 +3,47 @@
 
 int main(int argc, char const *argv[])
 {
+    cout << "Testando implementação fila com herança de lista encadeada:" << endl;
+    cout << "Os métodos da classe mão são utilizados de acordo com as regras de uma fila, ou seja, inserção no começo e remoção pelo fim." << endl;
+    // Instancia um objeto da classe fila
+    // O construtor da classe mãe é chamado
+    // "Por baixo dos panos", uma lista encadeada é incializada
     Queue queue;
-    List list;
-    for (int i = 0; i < 5; i++)
+    cout << endl << "Inserção no final da fila utilizando o método herdado da classe mãe(List):" << endl;
+    cout << "Inserindo 10 elementos(int) na fila: " << endl;
+    for (int i = 0; i < 10; i++)
     {
-        list.pushFront(i);
+        cout << "Inserindo " << i << " na fila..." << endl;
         queue.insert(i);
+        queue.printQueue();
     }
-    queue.printQueue();
-    cout << "Tamanho da fila = " << queue.size() << endl;
-    cout << "Primeiro da fila = " << queue.getFirst() << endl;
-    cout << "Ultimo da fila = " << queue.getLast() << endl;
-    queue.remove();
-    queue.printQueue();
+    cout << endl;
+    cout << "Remoção no começo da fila utilizando o método herdado da classe mãe(List):" << endl;
+    cout << "Removendo metade dos elementos da fila: " << endl;
+    for (int i = queue.size()/2; i > 0; i--)
+    {
+        cout << "Removendo... " << endl;
+        queue.remove();
+        queue.printQueue();
+    }
+    
+    cout << endl << "Primeiro elemento da fila: " << queue.getFirst() << endl;
+    cout << "Ultimo elemento da fila: " << queue.getLast() << endl;
 
+    cout << endl << "Removendo todos os elementos da fila: " << endl;
+    while (true)
+    {
+        if (queue.emptyQueue())
+        {
+            cout << "Fila vazia!" << endl;
+            break;
+        }
+        cout << "Removendo... " << endl;
+        queue.remove();
+        queue.printQueue();
+    }
+    cout << "Destruindo a fila utilizando o destrutor da classe mãe..." << endl;
+    queue.~Queue();
+    cout << "Fila destruída!" << endl;
     return 0;
 }
--- grafo/main.c
+++ grafo/main.c
@@ -4,25 +4,65 @@
 
 int main(int argc, char const *argv[])
 {
+    int i;
+    printf("Testando a implementação de grafo através de lista de adjacências:\n\n");
+    printf("Inicializando um grafo...\n");
     Graph G;
-    
     initGraph(&G);
-    insertVertex(&G);
-    insertVertex(&G);
-    insertVertex(&G);
-    insertVertex(&G);
+    if(G.array != NULL)
+        printf("Grafo inicializado!\n");
+    
+    printf("\nRepresentação do grafo: \n");
+    printf("v = vértice | p = peso\n");
+
+    printf("\nInserindo 4 vértices no grafo...\n");
+    for (i = 0; i < 4; i++)
+    {
+        printf("Inserindo vértice %d...\n", i);
+        insertVertex(&G);
+    }
+
+    printf("\nInserindo arestas para testes e exibindo o grafo...\n");
     insertAresta(&G, 0, 2, 8);
     insertAresta(&G, 0, 1, 7);
+    insertAresta(&G, 0, 3, 5);
     insertAresta(&G, 1, 2, 1);
+    insertAresta(&G, 1, 3, 3);
     insertAresta(&G, 2, 0, 5);
     printGraph(&G);
+
+    printf("\nVerificando se existe uma aresta do vértice 0 para o vértice 2:\n");
     if(existsAresta(&G, 0, 2))
         printf("Aresta existe!\n");
-    printf("Removendo aresta:\n");
-    removeAresta(&G, 2, 0);
+    else
+        printf("Aresta não existe!\n");
+    printf("Verificando se existe uma aresta do vértice 3 para o vértice 0:\n");
+    if(existsAresta(&G, 3, 0))
+        printf("Aresta existe!\n");
+    else
+        printf("Aresta não existe!\n");
+    printf("\nRemovendo aresta do vértice 2 para o vértice 0:\n");
+    if(removeAresta(&G, 2, 0))
+        printf("Aresta removida!\n");
+    else
+        printf("Falha ao remover aresta!\n");
     printGraph(&G);
-    printf("Deletando grafo\n");
+    printf("Removendo aresta do vértice 0 para o vértice 1:\n");
+    if(removeAresta(&G, 0, 1))
+        printf("Aresta removida!\n");
+    else
+        printf("Falha ao remover aresta!\n");
+    printGraph(&G);
+    printf("Removendo aresta do vértice 2 para o vértice 1:\n");
+    if(removeAresta(&G, 2, 1))
+        printf("Aresta removida!\n");
+    else
+        printf("Falha ao remover aresta!\n");
+    printGraph(&G);
+    
+    printf("\nDeletando grafo...\n");
     freeGraph(&G);
-    printGraph(&G);
+    printf("Grafo deletado!\n");
+    
     return 0;
 }
